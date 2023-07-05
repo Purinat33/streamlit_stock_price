1e12b54749135d7a49ed113a04396825200eb58f
Purinat33 <Purinat33@gmail.com>
2023-07-05 23:24:34 +0700

    Add image

--- mnist_user.py
+++ mnist_user.py
@@ -3,7 +3,9 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import os
+
+# import os
+import io
 
 st.title("MNIST Handwritten Digit Prediction")
 st.write("Now you can run MNIST yourself!")
@@ -12,10 +14,18 @@ st.divider()
 # IMPORTING THE MODEL
 # Path starts at ~/users/user_name for some reasom
 # Update: File path for opening in Streamlit and for running in normal python are DIFFERENT
-filepath = os.path.abspath("./Desktop/youtube/streamlit/my_mnist.h5")
-model = keras.models.load_model(filepath)
+
+# Load locally
+# filepath = os.path.abspath("./Desktop/youtube/streamlit/my_mnist.h5")
+# model = keras.models.load_model(filepath)
+
+# Load from GitHub
+# Note that the file URL needs to be for the `raw` file
+url = "https://github.com/Purinat33/streamlit_stock_price/raw/master/my_mnist.h5"
+file_path = keras.utils.get_file("my_mnist.h5", origin=url)
+model = keras.models.load_model(file_path)
+# Load
 
 st.header("Model Overview")
-model.summary(print_fn=lambda x: st.write(x))
 
 selected = st.selectbox("Select Digit [0-9]", [i for i in range(10)])
